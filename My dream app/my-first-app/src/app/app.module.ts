69c17409953fbf2a9440bbd2276e06f9305e1a66
theGeekkit <sonofgondolin16@gmail.com>
2022-09-21 19:58:47 -0500

    9-21-22

--- My dream app/my-first-app/src/app/app.module.ts
+++ My dream app/my-first-app/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './Server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
