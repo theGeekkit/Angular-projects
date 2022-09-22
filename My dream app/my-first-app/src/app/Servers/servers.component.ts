8a645a6e82a74660f70b46ec642542fa264d6344
theGeekkit <sonofgondolin16@gmail.com>
2022-09-21 21:41:56 -0500

    9-21-22

--- My dream app/my-first-app/src/app/Servers/servers.component.ts
+++ My dream app/my-first-app/src/app/Servers/servers.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): {
   }
 
 }
